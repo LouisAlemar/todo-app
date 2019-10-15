d7167609520760f45080e305e43af1e40f3a88eb
louisalemar <louisalemar@gmail.com>
2019-10-14 22:00:10 -0400

    add deleteTodo function

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 import Todos from '../Todos';
 
 class App extends Component {
-    state = { 
+    state = {
         todos: [
-            {id: 1, content: 'buy some mild'},
-            {id: 2, content: 'play mario kart'}
+            { id: 1, content: 'buy some mild' },
+            { id: 2, content: 'play mario kart' }
         ]
-     }
+    }
 
-    render() { 
-        return ( 
-            <div className="app">
+    deleteTodo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id;
+        })
+
+        this.setState({
+            todos
+        })
+    }
+
+    render() {
+        return (
+            <div className="todo-app container">
                 <h1 className="center blue-text">Todo's</h1>
-                <Todos todos={this.state.todos} />
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
             </div>
-         );
+        );
     }
 }
- 
+
 export default App;
--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 
 class Todos extends Component {
-    state = {  }
-    render() { 
-        const { todos } = this.props;
+    state = {}
+    render() {
+        const {
+            todos,
+            deleteTodo
+        } = this.props;
+
         const todoList = todos.length ? (
             todos.map(todo => {
-                return(
+                return (
                     <div className="collection-item" key={todo.id}>
-                        <span>{todo.content}</span>
+                        <span onClick={() => { deleteTodo(todo.id) }}>{todo.content}</span>
                     </div>
                 )
             })
         ) : (
-            <p className="center">You have no todos left!</p>
-        );
-        return ( 
+                <p className="center">You have no todos left!</p>
+            );
+        return (
             <div className="todos collection">
                 {todoList}
             </div>
-         );
+        );
     }
 }
- 
+
 export default Todos;
